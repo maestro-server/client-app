241bf0eccc593c0f5f6ee33d00381e3dc78d051f
Felipe Signorini <felipeklerk@yahoo.com.br>
2017-01-01 12:49:25 -0200

    callbacks - logins

---
+++ src/resources/factories/auth.js
@@ -0,0 +1,15 @@
+
+import Factory from './factory'
+
+class Users extends Factory {
+
+  constructor() {
+    super()
+
+    this.entity = '/users'
+    return this
+  }
+
+}
+
+export default Users
