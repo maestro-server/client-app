5726bb1ffdf87b0421d6aab49950c3c4c51d47b9
Felipe Klerk Signorini <felipe.signorini@b2wdigital.com>
2017-09-14 19:04:43 -0300

    refactoring single server

---
+++ src/mixins/view-single.js
@@ -0,0 +1,59 @@
+'use strict'
+
+import _ from 'lodash'
+import Login from 'services/login'
+import formatAdminer from 'src/resources/libs/formatAdminerData'
+
+export default {
+  data: function () {
+    return {
+      options: {
+        headers: {Authorization: Login.Authorization()},
+        responseAdapter: (resp) => ({
+            data: this.prepared(resp.data.items),
+            count: resp.data.found
+          }
+        )
+      }
+    }
+  },
+
+  computed: {
+    url() {
+      const url = this.entity.getUrl()
+      return `${API_URL}${url}`
+    }
+  },
+
+  methods: {
+    viewReducer(old, obj, key, fielder) {
+      const str = key > 0 ? "|" : ""
+      return `${old} ${str} ${obj[fielder]}`;
+    },
+
+    editP(data) {
+      this.$parent.editE(data)
+    },
+
+    deleteP(data) {
+      this.$parent.deleteE(data)
+    },
+
+    fetchAdminer(e) {
+      const data = formatAdminer(e)
+      _.forEach(
+        this.options.listColumns,
+        (val, key) => this.fetchData(key, key)(data, '')
+      )
+    },
+
+    fetchData(opt, fielder='data.items') {
+      return (result, getter='name') => {
+        const data = _.get(result, fielder)
+        if (!_.isEmpty(data)) {
+          this.options.listColumns[opt] = data.map(item => ({text: _.get(item, getter, item)}))
+        }
+      }
+    }
+  }
+}
