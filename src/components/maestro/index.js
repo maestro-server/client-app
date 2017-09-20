84b8dd2ec07fbc85037527d34309077031c8989e
Felipe Signorini <felipeklerk@yahoo.com.br>
2017-09-20 00:50:26 -0300

    modal acl

--- src/components/maestro/index.js
+++ src/components/maestro/index.js
@@ -1,16 +1,18 @@
 'use strict'
+
+import createrConfigList from './createrConfigList/CreaterConfigList.vue'
+import headerViewEntity from './headerViewEntity/HeaderViewEntity.vue'
+import headerEntity from './headerEntity/HeaderEntity.vue'
+import createrList from './createrList/CreaterList.vue'
+import treeViewItem from './jsontree/TreeViewItem.vue'
+import modalDelete from './modalDelete/delete.vue'
+import infoView from './infoView/InfoView.vue'
 import callback from './callback/Callback.vue'
 import uploader from './uploader/Uploader.vue'
+import modalACL from './modalACL/create.vue'
+import listBox from './listBox/ListBox.vue'
 import loader from './loader/Loader.vue'
 import imager from './imager/Imager.vue'
-import treeViewItem from './jsontree/TreeViewItem.vue'
-import createrList from './createrList/CreaterList.vue'
-import infoView from './infoView/InfoView.vue'
-import createrConfigList from './createrConfigList/CreaterConfigList.vue'
-import listBox from './listBox/ListBox.vue'
-import headerEntity from './headerEntity/HeaderEntity.vue'
-import headerViewEntity from './headerViewEntity/HeaderViewEntity.vue'
-import modalDelete from './modalDelete/delete.vue'
 
 let components = {
   createrConfigList,
@@ -22,6 +24,7 @@ let components = {
   infoView,
   callback,
   uploader,
+  modalACL,
   listBox,
   loader,
   imager
