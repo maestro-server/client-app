bdb6c9ca1b84bf29c1f49eb68e437e712f78172c
Felipe Klerk Signorini <felipe.signorini@b2wdigital.com>
2017-10-10 20:04:51 -0300

    home

---
+++ src/pages/dashboard/home/components/servers_widget.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="col-sm-3 col-xs-6">
+    <ul class="list-group">
+      <li class="list-group-item" v-for="server in servers">
+        <router-link :to="{name: 'servers.single', params: { id: server._id }}">
+          <b>{{server.hostname}}</b> <span v-if='server.os'>({{server.os.base}})</span>
+          <span v-if="server.servercenters"> - {{server.servercenters.name}}</span>
+        </router-link>
+        <br/>
+        <bs-label type='default'>{{server.ipv4_private}}</bs-label>
+        <bs-label type='default'>{{server.ipv4_public}}</bs-label>
+        <bs-label type='success'>{{server.role}}</bs-label>
+        <bs-label type='success'>{{server.environment}}</bs-label>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  'use strict'
+
+  export default {
+    props: {
+      servers: {}
+    }
+  }
+</script>
--- src/pages/dashboard/home/script.js
+++ src/pages/dashboard/home/script.js
@@ -1,9 +1,71 @@
 'use strict'
+
+import _ from 'lodash'
+import {mapActions} from 'vuex'
+import doughnutChart from './components/doughnut_chart.vue'
+import barChart from './components/bar_chart.vue'
+import serverWidget from './components/servers_widget.vue'
+
+import FectherEntity from 'services/fetchEntity'
+import Servers from 'factories/servers'
+import Applications from 'factories/applications'
+import Datacenters from 'factories/datacenters'
+
 export default {
   name: 'home',
+
+  components: {
+    doughnutChart,
+    barChart,
+    serverWidget
+  },
+
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      result: {servers:[]},
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: ["#2B8B7E", "#FCDA0E", "#2B69AC", "#0E0184", "#5B5DC2", "#D21E94", "#8CA445", "#280AF0", "#3F6DEC", "#91FC1A", "#41923C", "#9B04BD", "#FF7920", "#89DD6E", "#6F7C71", "#F2F1A2"],
+            data: [40, 39, 10, 40, 39, 80, 40]
+          }
+        ]
+      }
     }
+  },
+
+  methods: {
+    makeChart(post, entity) {
+      const {data} = post
+      this.result[entity.name.toLowerCase()] = data
+    },
+
+    getLast(entity, offset = 0, limit = 5) {
+      const result = _.get(this.result, entity+'.items', [])
+      if(_.isArray(result)) {
+        return  _.slice(result, offset, (offset+limit))
+      }
+    },
+
+    ...mapActions([
+      'setPage' // map this.increment() to this.$store.dispatch('increment')
+    ]),
+
+    fetchData (entity, force=false) {
+      FectherEntity(entity)({force})
+        .find(e=>this.makeChart(e, entity))
+    },
+  },
+
+  mounted () {
+    this.setPage([
+      'Overview',
+      'A simple application to manage an IT operations team servers, including systems, applications and services.',
+      'fa-cloud'])
+    this.fetchData(Servers)
+    this.fetchData(Applications)
+    this.fetchData(Datacenters)
   }
 }
