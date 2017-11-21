71c258b8d1275b4f7143575f0ac1360f8a0fef0a
Felipe Klerk Signorini <felipe.signorini@b2wdigital.com>
2017-11-21 17:59:43 -0200

    dockerfile - add env

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,15 @@ MAINTAINER Felipe Signorini <felipe.signorini@maestroserver.io>
 
 EXPOSE 80 443
 
+RUN apk add --no-cache tini
+
 COPY nginx.conf /etc/nginx/nginx.conf
 COPY ./dist /var/www
 COPY ./static /var/www/static
-COPY run_start_container.sh /opt/run_start_container.sh
+
+ADD ./run_start_container.sh /opt/run_start_container.sh
+
+RUN chmod +x /opt/run_start_container.sh
+
+ENTRYPOINT ["/sbin/tini","-g","--"]
+CMD ["/opt/run_start_container.sh"]
--- run_start_container.sh
+++ run_start_container.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-if [[ -z "${API_URL}" ]]; then
-  API_URL="http://localhost:8888"
-fi
-
-if [[ -z "${API_TIMEOUT}" ]]; then
-  API_TIMEOUT=5000
-fi
-
-if [[ -z "${STATIC_URL}" ]]; then
-  STATIC_URL="/upload/"
-fi
-
-sed "s#api_url content=[[:alnum:]\/:]*#api_url content=$API_URL#" /var/www/index.html
-sed "s/#TIMEOUT#/$API_TIMEOUT/g" /var/www/index.html
-sed "s/#UPLOAD#/$STATIC_URL/g" /var/www/index.html
+VURL=${API_URL:='http://localhost:8888'}
+sed -i "s#api_url content=[[:alnum:]\/:]*#api_url content=$VURL#" /var/www/index.html
+nginx -g 'daemon off;'
