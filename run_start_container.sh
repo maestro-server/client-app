675c7520098e541974eb9857850cc2687a925a49
Felipe Signorini <felipeklerk@yahoo.com.br>
2018-02-22 01:24:42 -0300

    dot env file

--- run_start_container.sh
+++ run_start_container.sh
@@ -3,5 +3,5 @@
 VURL=${API_URL:='http://localhost:8888'}
 sed -i "s#api_url content=[[:alnum:]\/:]*#api_url content=$VURL#" /var/www/index.html
 VSTATIC=${STATIC_URL:='/upload/'}
-sed -i "s#static_url content=[[:alnum:]\/:]*#static_url content=$VSTATIC#" /var/www/index.html
+sed -i "s#static_url content=[[:alnum:]\/:\.]*#static_url content=$VSTATIC#" /var/www/index.html
 nginx -g 'daemon off;'
