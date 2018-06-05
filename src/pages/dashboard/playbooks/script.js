223302ed77ae9f140b59f6121cc25ff8a45b069a
Felipe Signorini <felipeklerk@yahoo.com.br>
2018-06-04 23:45:44 -0300

    hidde unfinished features

--- src/pages/dashboard/playbooks/script.js
+++ src/pages/dashboard/playbooks/script.js
@@ -5,8 +5,7 @@ import tabMenu from '../_modules/tabMenu/tab-menu.vue'
 export default {
   data () {
     return {
-      submenu: {'jobs': 'Jobs', 'playbook': 'Playbooks', 'task-template': 'Task Template',
-        'projects':'Projects', 'scheduler': 'Schedule', 'access-manager':'Access Manager'},
+      submenu: {'scheduler': 'Schedule'},
       reference: null
     }
   },
@@ -23,9 +22,9 @@ export default {
 
   mounted() {
     this.setPage([
-      'Cloud Playbooks',
-      'Setup, run, schedule and audit playbooks.',
-      'icon-ansible'
+      'Schedulers',
+      'Setup, run, schedulers and jobs.',
+      'fa fa-calendar'
     ])
   }
 }
