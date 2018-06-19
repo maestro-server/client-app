43b73bb28743a5eab8265f59481442ee8dca0bc3
Felipe Signorini <felipeklerk@yahoo.com.br>
2018-06-19 15:10:51 +0000

    bugfix - filters datacenters

--- src/pages/dashboard/inventory/snapshot/list/table/script.js
+++ src/pages/dashboard/inventory/snapshot/list/table/script.js
@@ -16,6 +16,9 @@ export default {
         filterable: ['name', 'ldatacenters', 'vpc_id', 'family'],
         listColumns: {
           ldatacenters: []
+        },
+        headings: {
+          ldatacenters: "Datacenters"
         }
       }
     }
