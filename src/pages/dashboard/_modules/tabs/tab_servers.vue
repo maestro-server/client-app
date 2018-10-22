6fa7f1219e277a69a21800766a5b29f7cc200049
fsignorini <fsignorini@rdisoftware.com>
2018-10-22 20:23:58 +0000

    bugfix - tab servers - base var

--- src/pages/dashboard/_modules/tabs/tab_servers.vue
+++ src/pages/dashboard/_modules/tabs/tab_servers.vue
@@ -39,7 +39,7 @@
     </template>
 
     <template slot="view" slot-scope="props">
-      <b class="text-capitalize">{{props.item.hostname}}</b> <span v-if='props.item.os.base'>({{props.item.os.base}})</span><span v-if='props.item.datacenters'> - {{props.item.datacenters.name}}</span><br/>
+      <b class="text-capitalize">{{props.item.hostname}}</b> <span v-if='props.item.os && props.item.os.base'>({{props.item.os.base}})</span><span v-if='props.item.datacenters'> - {{props.item.datacenters.name}}</span><br/>
       <span class='ft15'>
           <bs-label type='default'>{{props.item.ipv4_private}}</bs-label>
           <bs-label type='default'>{{props.item.ipv4_public}}</bs-label>
