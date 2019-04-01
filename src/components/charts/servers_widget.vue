f80f663ba6e34a877b97634390553bc457995f1f
Felipe Signorini <felipeklerk@yahoo.com.br>
2019-04-01 22:43:01 +0100

    bugfix - widgets

--- src/components/charts/servers_widget.vue
+++ src/components/charts/servers_widget.vue
@@ -2,27 +2,21 @@
   <div class="col-sm-3 col-xs-12">
     <bs-list>
       <li
-v-for="server in servers"
-:key
-class="list-group-item word-break"
->
+        v-for="server in servers"
+        :key="server.hostname"
+        class="list-group-item word-break"
+      >
         <router-link :to="{name: 'servers.single', params: { id: server._id }}">
           <b>{{ server.hostname }}</b> <span v-if="server.os && server.os.base">({{ server.os.base }})</span>
           <span v-if="server.datacenters"> - {{ server.datacenters.name }}</span>
         </router-link>
         <br>
-        <bs-label type="default">
-{{ server.ipv4_private }}
-</bs-label>
-        <bs-label type="default">
-{{ server.ipv4_public }}
-</bs-label>
-        <bs-label type="success">
-{{ server.role }}
-</bs-label>
-        <bs-label type="success">
-{{ server.environment }}
-</bs-label>
+        <div class="f15">
+          <bs-label type="default" v-if="server.ipv4_private">{{ server.ipv4_private }}</bs-label>
+          <bs-label type="default" v-if="server.ipv4_public">{{ server.ipv4_public }}</bs-label>
+          <bs-label type="success" v-if="server.role">{{ server.role }}</bs-label>
+          <bs-label type="success" v-if="server.environment">{{ server.environment }}</bs-label>
+        </div>
       </li>
     </bs-list>
   </div>
@@ -39,8 +33,8 @@ class="list-group-item word-break"
 </script>
 
 <style lang="scss">
-.word-break {
-  word-wrap: break-word;
-}
+  .word-break {
+    word-wrap: break-word;
+  }
 </style>
 
