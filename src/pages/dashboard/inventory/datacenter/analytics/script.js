d893afd038deedb43d6bc40f11314d255968cf05
Felipe Signorini <felipeklerk@yahoo.com.br>
2019-04-17 19:55:01 +0100

    wip - dash

--- src/pages/dashboard/inventory/datacenter/analytics/script.js
+++ src/pages/dashboard/inventory/datacenter/analytics/script.js
@@ -1,16 +1,35 @@
 'use strict'
 import _ from 'lodash'
 import Datacenters from 'factories/datacenters'
+import Reports from 'factories/reports'
 import ViewSingle from 'mixins/view-single'
 import FectherEntity from 'services/fetchEntity'
+import charts from 'mixins/charts'
+import pieChart from 'components/charts/pie_chart.vue'
+import doughnutChart from 'components/charts/doughnut_chart.vue'
+import totalChart from 'components/charts/total_chart.vue'
+import polarChart from 'components/charts/polar_chart.vue'
+import barChart from 'components/charts/bar_chart.vue'
 
 export default {
-  mixins: [ViewSingle],
+  mixins: [ViewSingle, charts],
+
+  components: {
+    pieChart,
+    totalChart,
+    doughnutChart,
+    polarChart,
+    barChart
+  },
 
   data: function () {
     return {
       entity: Datacenters,
-      model: {}
+      model: {},
+      analytics: {
+        servers: {},
+        applications: {}
+      }
     }
   },
 
@@ -28,6 +47,24 @@ export default {
 
     finishReport(data) {
       this.finishJob(data);
+    },
+
+    updateAnalyticsInfo() {
+      const reports = _.get(this.model, 'reports')
+      if(reports) {
+
+        _.forEach(reports, (report) => {
+          FectherEntity(Reports)({force: true})
+            .findOne((response) => {
+              console.log(response)
+            }, _.get(report, '_id'))
+        })
+
+      }
     }
+  },
+
+  created () {
+    this.$on('finishFetchData', this.updateAnalyticsInfo)
   }
 }
