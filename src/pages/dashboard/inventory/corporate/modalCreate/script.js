e09f9958e3dfca45039c876acab5475be33c44c5
Felipe Signorini <felipeklerk@yahoo.com.br>
2020-04-07 20:20:34 +0100

    add corporate applications

---
+++ src/pages/dashboard/inventory/corporate/modalCreate/script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+import Modals from 'mixins/modals'
+import ModalsApps from 'mixins/modals-apps'
+
+
+export default {
+  mixins: [Modals, ModalsApps],
+
+  data () {
+    return {
+      family: 'Corporate',
+      own: 1,
+      initialData: {
+        name: null, description: null, provider:null, datacenters: {},
+        tags: [], role: {endpoint: null}
+      },
+      mapper: [
+        {name: 'endpoint', label: 'Endpoint', validate: 'url'},
+        {name: 'extra_config', label: 'Extra Configs', type: "textarea", validate: 'min:2'}
+      ]
+    }
+  }
+
+}
---
+++ src/pages/dashboard/inventory/corporate/view/router.js
@@ -0,0 +1,8 @@
+'use strict'
+import comp from './view'
+
+export default {
+  name: 'corporate.single',
+  path: 'single/:id',
+  component: comp
+}
