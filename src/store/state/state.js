ff778606c56495c7290d515e80b9ff3f963848b1
Felipe Signorini <felipeklerk@yahoo.com.br>
2020-04-11 13:29:02 +0100

    refactoring a bash replacing vars

--- src/store/state/state.js
+++ src/store/state/state.js
@@ -7,6 +7,7 @@ import CacheManager from 'services/cacheManager'
  * Default init state
  *
  */
+
 export default {
   version: VERSION,
 
@@ -25,12 +26,12 @@ export default {
   cache: {},
 
   options: {
-    'base_url': `${window.location .protocol}//${window.location.host}`,
-    'api_url': _.get(document.head.querySelector("[name=api_url]"), 'content', 'http://localhost:8888'),
-    'analytics_url': _.get(document.head.querySelector("[name=analytics_url]"), 'content', 'http://localhost:9999'),
-    'websocket_url': _.get(document.head.querySelector("[name=websocket_url]"), 'content', 'ws://localhost:8000'),
-    'static_url': _.get(document.head.querySelector("[name=static_url]"), 'content', '/upload/'),
-    'logo_url': _.get(document.head.querySelector("[name=logo_url]"), 'content', '/static/imgs/logo300.png'),
+    'base_url': `${window.location.protocol}//${window.location.host}`,
+    'api_url': _.get(document.head.querySelector("[name=api_url]"), 'content', `//${window.location.hostname}:8888`),
+    'analytics_url': _.get(document.head.querySelector("[name=analytics_url]"), 'content', `//${window.location.hostname}:9999`),
+    'websocket_url': _.get(document.head.querySelector("[name=websocket_url]"), 'content', `ws://${window.location.hostname}:8000`),
+    'static_url': _.get(document.head.querySelector("[name=static_url]"), 'content', `//${window.location.hostname}:8888/static/`),
+    'logo_url': _.get(document.head.querySelector("[name=logo_url]"), 'content', `//${window.location.hostname}:8888/static/imgs/logo300.png`),
     'theme': _.get(document.head.querySelector("[name=theme]"), 'content', 'lotus'),
     'api_timeout': 5000
   }
