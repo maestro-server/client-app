0de11c6edf6e5efcec4950cb3be5ef277bd89402
Felipe Signorini <felipeklerk@yahoo.com.br>
2020-04-12 12:04:55 +0100

    fix api autodiscovery

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -74,7 +74,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true,
       minify: {
-        removeComments: true,
+        removeComments: false,
         collapseWhitespace: true,
         removeAttributeQuotes: true
         // more options:
