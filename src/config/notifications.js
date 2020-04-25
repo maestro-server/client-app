75216fb9ce5ac1c730687442cc16f766160d430e
Felipe Signorini <felipeklerk@yahoo.com.br>
2020-04-25 17:27:27 +0100

    fixing no data on table

--- src/config/notifications.js
+++ src/config/notifications.js
@@ -1,7 +1,7 @@
 'use strict'
 import iziToast from 'izitoast'
 
-const toast = ({ title, message, type, timeout, position = 'topRight' }) => iziToast[type]({ title, message, timeout, position })
+const toast = ({ title, message, type, timeout, position = 'topRight', displayMode = 2 }) => iziToast[type]({ title, message, timeout, position, displayMode })
 
 export default {
   success: toast,
